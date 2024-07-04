c67d9ac95dfe4c5ca17f39dd1a4885d901eb581a
beknazar <beknazarbekazizov@gmail.com>
2024-07-04 11:08:17 +0500

    exam tasks

--- shop/urls.py
+++ shop/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from shop.views import index, product_detail,  logout
+from shop.views import index, product_detail, logout, add_comment, add_order
 
 urlpatterns = [
     path('online_shop/', index, name='home'),
-    path('home/',index,name='hom'),
+    # path('home/',index,name='hom'),
     path('category/<slug:category_slug>/products', index, name='products_of_category'),
     path('product_detail/<slug:slug>',product_detail,name='product_detail'),
     path('product_r/<slug:slug>products',product_detail,name='product_r'),
-    path('exspensive/<slug:slug>',product_detail,name='productex'),
+    path('add_comment/<slug:slug>',add_comment,name='add_comment'),
+    path('exspensive/<slug:slug>', product_detail, name='productex'),
+    path('add_order/<slug:slug>',add_order,name='add_order'),
     path('logout/',logout,name='logoutx'),
 
     ]
--- shop/views.py
+++ shop/views.py
@@ -16,58 +16,77 @@ from shop.models import Product, Category, Comment
 # @login_required
 def index(request, category_slug=None):
     search = request.GET.get('search')
-    if search:
-        products = Product.objects.filter(Q(name__icontains=search))[:4]
-    else:
-        products = Product.objects.all()
-
     categories = Category.objects.all()
-    # products = Product.objects.all()
-    hom=Product.objects.all()
+    products = Product.objects.all()
+    filter_expensive = request.GET.get('expensive')
+    filter_cheap = request.GET.get('cheap')
     if category_slug:
         products = products.filter(category__slug=category_slug)
+    elif search:
+        products = products.filter(Q(name__icontains=search))[:4]
+
+    elif filter_expensive:
+        products = products.order_by('-price')[:3]
+    elif filter_cheap:
+        products = products.order_by('price')[:3]
+    else :
+        products = Product.objects.all()
 
 
     context = {
         'products': products,
         'categories': categories,
-        'home' : hom
+        'filter_expensive': filter_expensive,
+        'filter_cheap': filter_cheap
+
     }
     return render(request, 'app/home.html', context)
 
 
-def product_detail(request,slug):
+def product_detail(request, slug):
     product = Product.objects.get(slug=slug)
     product_r = Product.objects.filter(category=product.category).exclude(slug=product.slug)
-    comment_list = Comment.objects.filter(product__slug=slug).order_by('-created_at')[:3]
-    commentForm = CommentModelForm()
-    orderForm = OrderModelForm()
+    comments = Comment.objects.filter(product__slug=slug).order_by('-created_at')[0:3]
+    count = comments.count()
+    context = {'product': product,
+                'comments': comments,
+                'count': count,
+                'product_r': product_r, }
+
+    return render(request, 'app/detail.html', context)
+
+
+
+def add_comment(request, slug):
+    product = Product.objects.get(slug=slug)
     new_comment = None
-    new_order = None
+    form = CommentModelForm()
     if request.method == 'POST':
-        commentForm = CommentModelForm(data=request.POST)
-        orderForm = OrderModelForm(data=request.POST)
-        if commentForm.is_valid():
-            new_comment = commentForm.save(commit=False)
+        form = CommentModelForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
             new_comment.product = product
             new_comment.save()
-        elif orderForm.is_valid():
-            new_order = orderForm.save(commit=False)
+            return redirect('product_detail', slug)
+    context = {'product': product, 'comment_form': form, 'new_comment': new_comment}
+    return render(request, 'app/detail.html', context)
+
+
+def add_order(request, slug):
+    new_order = None
+    product = Product.objects.get(slug=slug)
+    form = OrderModelForm()
+    if request.method == 'POST':
+        form = OrderModelForm(request.POST)
+        if form.is_valid():
+            new_order = form.save(commit=False)
             new_order.product = product
             new_order.save()
-
-    context = {
-        'product': product,
-        'product_r': product_r,
-        'comment_form': commentForm,
-        'comment_list': comment_list,
-        'orderform': orderForm,
-        'new_comment': new_comment,
-        'new_order': new_order,
-
-
-    }
+            return redirect('product_detail', slug)
+    context = {'form': form, new_order: new_order}
     return render(request, 'app/detail.html', context)
+
+
 
 
 
